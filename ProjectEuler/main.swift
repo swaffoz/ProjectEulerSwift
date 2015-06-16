af373cabba68bd8e5c03f3b63934567de9929e50
Zane Swafford <zane@zaneswafford.com>
2015-06-16 14:36:50 -0500

    Initial Commit

---
+++ ProjectEuler/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ProjectEuler
+//
+//  Created by Zane Swafford on 6/16/15.
+//  Copyright (c) 2015 Zane Swafford. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+

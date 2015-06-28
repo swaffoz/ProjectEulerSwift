8129bb484ddae729a8ab9a0b1a0425d8558f48d2
Zane Swafford <zane@zaneswafford.com>
2015-06-27 22:24:15 -0500

    Added Problem 11 and tests

---
+++ ProjectEulerTests/Problem11Tests.swift
@@ -0,0 +1,24 @@
+//
+//  Problem11Tests.swift
+//  ProjectEuler
+//
+//  Created by Zane Swafford on 6/27/15.
+//  Copyright (c) 2015 Zane Swafford. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class Problem11Tests: XCTestCase {
+
+    func testProblem11Correct() {
+        XCTAssert(problem11() == 70600674, "Problem 11 should equal 70600674")
+    }
+    
+    func testProblem11Performance() {
+        self.measureBlock() {
+            problem11()
+        }
+    }
+
+}
